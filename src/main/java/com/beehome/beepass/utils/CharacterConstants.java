fd91c2f62af5f49b506727cea5a2f6c1d8e65eac
ruj <214782@bk.ru>
2024-10-26 11:02:17 -0300

    chore: add character constants

---
+++ src/main/java/com/beehome/beepass/utils/CharacterConstants.java
@@ -0,0 +1,10 @@
+package com.beehome.beepass.utils;
+
+public class CharacterConstants {
+
+    public static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    public static final String NUMBERS = "0123456789";
+    public static final String SPECIAL_CHARS = "!@#$%^&*()-_+=<>?";
+
+}
